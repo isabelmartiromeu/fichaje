59a69f50c414122e37dbbbd2c26a93f1b2b0132c
isabelmartiromeu <lasillera74@gmail.com>
2024-05-04 19:07:36 +0000

    Aumenta el número de horas de libre disposición en la bolsa cuando se ficha la salida, si da lugar

--- models/registro_fichaje.py
+++ models/registro_fichaje.py
@@ -4,6 +4,8 @@ from odoo import models, fields, api
 from datetime import datetime, timedelta
 import math
 
+import pytz
+
 class registro_fichaje(models.Model):
     """
     Define un fichaje
@@ -15,9 +17,9 @@ class registro_fichaje(models.Model):
 
     fecha = fields.Date(string='Fecha', required = True,default=date.today(),readonly=True) #Debe tener la fecha de cuando se entra
     _order = 'fecha'
-
-    hora_entrada = fields.Char(string="Hora de entrada",readonly=True, required = True, default=lambda self:fields.Datetime.now().strftime('%H:%M:%S')) #Debe tener la hora de cuando se entra 
-    #hora_entrada = fields.Char(string="Hora de entrada",required = True) #Debe tener la hora de cuando se entra 
+    # EL BUENO ES ESTE DE ARRIBA
+    # hora_entrada = fields.Char(string="Hora de entrada",readonly=True, required = True, default=lambda self:fields.Datetime.now().strftime('%H:%M:%S')) #Debe tener la hora de cuando se entra 
+    hora_entrada = fields.Char(string="Hora de entrada",required = True) #Debe tener la hora de cuando se entra 
 
 
     hora_salida = fields.Char(string='Hora de salida') #No se debe poder rellenar
@@ -31,14 +33,46 @@ class registro_fichaje(models.Model):
     
 
     _sql_constraints = [
-        ('fecha_hora_entrada_uniq_fichaje', 'unique(fecha,hora_entrada)', 'La fecha y hora de entrada deben ser únicos'),
+        ('fecha_hora_empleado_uniq_fichaje', 'unique(fecha,hora_entrada,empleado_id)', 'La fecha y hora de entrada deben ser únicos para cada empleado'),
     ]
 
-
     def crea_fichaje_salida(self):
-        self.hora_salida = fields.Datetime.now().strftime('%H:%M:%S')
+    # Va a contralar si se han realizado horas de más para aumentar las horas
+    # de libre disposición en bolsa_horas.
+        if not self.hora_salida: #Si ya hay algo no se ejecuta el código
+            timezone_es = pytz.timezone('Europe/Madrid')
+            hora_actual = fields.Datetime.now(timezone_es)
+            # En formato string:
+            hora_salida_dt = hora_actual.strftime('%H:%M')
+            #En formato hora:
+            hora_salida_dt = datetime.strptime(hora_salida_dt, "%H:%M")
+
+            hora_entrada_dt = datetime.strptime(self.hora_entrada, "%H:%M")
+
+            # Restamos la hora de salida menos la hora de entrada
+            diferencia_horas = hora_salida_dt - hora_entrada_dt
+
+            diferencia_horas = (diferencia_horas.total_seconds() / 3600)  # Convertir a horas
+            #  Restamos una hora para comer, redondeando hacia abajo
+            diferencia_horas = math.floor(diferencia_horas) - 1
 
 
+            # Solo suma la diferencia de horas a la bolsa de horas si es mayor que 8
+            if diferencia_horas > 8:
+                if self.empleado_id:
+                    bolsa_horas_obj = self.env['fichaje.bolsa_horas']
+                    bolsa_horas = bolsa_horas_obj.search([('empleado_id', '=', self.empleado_id.id)], limit=1)
+                    if bolsa_horas:
+                        bolsa_horas.horas += diferencia_horas - 8
+                    else:
+                        bolsa_horas_obj.create({
+                            'empleado_id': self.empleado_id.id,
+                            'horas': diferencia_horas,
+                        })
+
+            # Actualizar la hora de salida en el registro de fichaje
+            self.hora_salida = fields.Datetime.now().strftime('%H:%M')
+        
     def imprimir_informe(self):
 	     return self.env.ref('mantenprev.registro_fichaje_pdf_report').report_action(self)
 
